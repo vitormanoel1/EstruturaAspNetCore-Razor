541f0e92d19c46f37c658ccd11daa4120dcf1849
vitor20022003 <vitorm4123@gmail.com>
2021-01-10 22:02:16 -0300

    visual alterado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Estrutura_AspNetCore_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
